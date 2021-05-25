091c7261329c28c01bb0cd0874df71f2474566cd
kushal <kushalsai2449@gmail.com>
2021-05-25 20:14:23 +0530

    organised everything

--- js/navigation.js
+++ js/navigation.js
@@ -1,21 +1,21 @@
 var username="";
 $(document).ready(function(){
-    $("#mainbody").load("home.html#content", homeload)
+    $("#mainbody").load("../pages/home.html#content", homeload)
     $(".navbutton").click(function(){
         $(".nav_drawer").toggleClass('out_nav');
         $(".nav_drawer").toggleClass('in_nav');
     })
 
     $("#homepage").click(function(){
-        $("#mainbody").load("home.html#content", homeload)
+        $("#mainbody").load("../pages/home.html#content", homeload)
 
     })
 
     $("#aboutpage").click(function(){
-        $("#mainbody").load("about.html#content")
+        $("#mainbody").load("../pages/about.html#content")
     })
     $("#servicepage").click(function(){
-        $("#mainbody").load("service.html#content")
+        $("#mainbody").load("../pages/service.html#content")
     })
 })
 
